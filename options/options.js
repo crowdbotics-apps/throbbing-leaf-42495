6cac83e1530f290c2ae16d93a50c46b3b6f2d376
Crowdbotics <team@crowdbotics.com>
2023-07-29 01:03:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "throbbing-leaf-42495",
+  url: "https://throbbing-leaf-42495.botics.co",
+  api: "https://throbbing-leaf-42495.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
